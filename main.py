0ec0d3b12085530a87704c97b1712c0017a7f27e
ThomasB3143 <tomblair3008@yahoo.com>
2024-10-28 22:20:11 +0000

    new file!

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from tkinter import *
+import node
 root = Tk()
 root.title("Circuit Sim")
 root.geometry('1200x600')
 board = Canvas(root, bg="gray", height=400, width=400)
 board.place(x=0,y=0)
-
-
 root.mainloop()
---
+++ node.py
@@ -0,0 +1,10 @@
+import math
+def words():
+    print("hello")
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def distanceFrom(n):
+        return math.sqrt((x - n.x)^2 + (y - n.y)^2)
+words()
