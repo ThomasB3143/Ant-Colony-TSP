9b359986c16ab6a51946190737d3c8d50db6b6c6
Thomas Blair <tomblair3008@yahoo.com>
2024-10-30 13:36:18 +0000

    started simulator

--- node.py
+++ node.py
@@ -1,10 +1,7 @@
 import math
-def words():
-    print("hello")
 class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     def distanceFrom(self, n2):
         return math.sqrt((self.x - n2.x)^2 + (self.y - n2.y)^2)
-words()
--- simulation.py
+++ simulation.py
@@ -1,4 +1,18 @@
 from node import *
 node1 = Node(x=10,y=10)
 node2 = Node(x=20,y=20)
-print(node1.distanceFrom(node2))
+node3 = Node(x=40,y=20)
+def begin(nodeList):
+
+    pheremoneMap = [[] for i in range(len(nodeList))]*len(nodeList)
+    distanceMap = [[] for i in range(len(nodeList))]*len(nodeList)
+
+    # populate distanceMap
+    for i in range(0,len(nodeList)-1):
+        print("i = ",i)
+        for j in range(i+1,len(nodeList)-1):
+            print("j = ",j)
+            distanceMap[i][j] = nodeList[i].distanceFrom(nodeList[j])
+            distanceMap[j][i] = nodeList[i].distanceFrom(nodeList[j])
+    print(distanceMap)
+begin([node1,node2,node3])
