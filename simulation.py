9eb51d9051cd6f733e78552906f8f101928f1176
ThomasB3143 <tomblair3008@yahoo.com>
2024-11-05 22:31:38 +0000

    gettting better!

--- simulation.py
+++ simulation.py
@@ -1,8 +1,10 @@
 from node import *
-APC = 400 # constant - added pheromones coefficient
-PDC = 0.6 # constant - pheromone decay constant
-PPE = 3 # constant - pheromone priority exponent
+APC = 20 # constant - added pheromones coefficient
+PDC = 0.9 # constant - pheromone decay constant
+PPE = 0.9 # constant - pheromone priority exponent
 DPE = -1.5 # constant - distance priority exponent
+bestPath = []
+bestDistance = -1
 def setup(nodeList): # sets up initial matrices and carries out antRun() repeatedly
     # create pheromone and distance matrices
     global pheromoneMap
@@ -19,30 +21,24 @@ def setup(nodeList): # sets up initial matrices and carries out antRun() repeate
         print(distanceMap[i])
 
 def antRun():
-    print("BEGGINING ANT RUN")
+    global bestPath
+    global bestDistance
     unvisited = list(range(1,len(distanceMap[0]))) # list of all univisited nodes (0 already removed)
     visited = [0] # will be appended in the order of nodes visited during the cycle
-    print("univisted: ",unvisited)
     currentNode = 0
     totalDistance = 0
     while unvisited: # while the unvisited list is not empty
         desireList = []
-        print("currentNode: ",currentNode)
-        print("unvisited in loop: ",unvisited)
-        print("visited in loop: ",visited)
         for n in unvisited: # calculates desire for each unvisited node
             desireList.append(desireBetween(currentNode,n))
-        print("desireList in loop: ",desireList)
-
         chosenNode = choosePath(unvisited,desireList) # REPLACE WITH PROBABILITY FUNCTION!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        print("chosen node is: ",chosenNode)
         totalDistance += distanceMap[currentNode][chosenNode] # adds distance onto total for later pheromone addition
         currentNode = chosenNode # changes current node to desired node
         visited.append(chosenNode) # adds new node to visited
         unvisited.remove(chosenNode) # removes new node from unvisited
-    print(visited)
-    print(totalDistance)
-    # path is now made, write the pheromone secretion code here
+    if bestDistance == -1 or totalDistance < bestDistance:
+        bestPath = visited
+        bestDistance = totalDistance
     updatePheromones(visited, totalDistance)
     for i in range(len(pheromoneMap)):
         print(numpy.round(pheromoneMap[i],4))
@@ -79,5 +75,7 @@ node8 = Node(x=72,y=298)
 node9 = Node(x=799,y=527)
 setup([node0,node1,node2,node3,node4,node5,node6,node7,node8,node9])
 # optimal path is 0 4 3 9 7 1 5 2 8 6
-for i in range(200):
+# ants are going like 
+for i in range(400):
     antRun()
+print(bestPath)
